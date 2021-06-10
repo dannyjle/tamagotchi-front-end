7248431c98616009b84c10a12be75692d13020ff
Danny Le <le.dannyj@gmail.com>
2021-06-10 15:54:00 -0400

    Itterated through pets via api

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,23 +1,41 @@
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export function Home() {
+  const [pets, setPets] = useState([])
+
+  useEffect(function () {
+    async function loadPets() {
+      const response = await fetch('https://digimonapi.herokuapp.com/api/Pets')
+
+      if (response.status === 200) {
+        const json = await response.json()
+        setPets(json)
+      }
+    }
+
+    loadPets()
+  }, [])
+
   return (
     <>
-      <div className="pets">
-        <h2>Gatomon</h2>
-        <p></p>
-        <dl className="status">
-          <dt className="statusTitle">Birthday:</dt>
-          <dd>-</dd>
-          <dt className="statusTitle">Hunger Level:</dt>
-          <dd>-</dd>
-          <dt className="statusTitle">Happiness Level:</dt>
-          <dd>-</dd>
-        </dl>
-        <Link to="/interactions/">
-          <button>Interact</button>
-        </Link>
-      </div>
+      {pets.map(pet => (
+        <div key={pet.id} className="pets">
+          <h2>{pet.name}</h2>
+          <p></p>
+          <dl className="status">
+            <dt className="statusTitle">Birthday:</dt>
+            <dd className="birthday">{pet.birthday}</dd>
+            <dt className="statusTitle">Hunger Level:</dt>
+            <dd>{pet.hungerLevel}</dd>
+            <dt className="statusTitle">Happiness Level:</dt>
+            <dd>{pet.happinessLevel}</dd>
+          </dl>
+          <Link to="/interactions/">
+            <button>Interact</button>
+          </Link>
+        </div>
+      ))}
 
       <div className="create">
         <h2>Create New Digimon:</h2>
--- src/components/Interactions.jsx
+++ src/components/Interactions.jsx
@@ -1,28 +1,47 @@
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import { Home } from './Home'
 
 export function Interactions() {
+  const [pets, setPets] = useState([])
+
+  useEffect(function () {
+    async function loadPets() {
+      const response = await fetch('https://digimonapi.herokuapp.com/api/Pets')
+
+      if (response.status === 200) {
+        const json = await response.json()
+        setPets(json)
+      }
+    }
+
+    loadPets()
+  }, [])
+
   return (
     <>
       <Link to="/">
         <button className="homeButton">Home</button>
       </Link>
-      <div className="pets">
-        <h2>Gatomon</h2>
-        <p></p>
-        <dl className="status">
-          <dt className="statusTitle">Birthday:</dt>
-          <dd>-</dd>
-          <dt className="statusTitle">Hunger Level:</dt>
-          <dd>-</dd>
-          <dt className="statusTitle">Happiness Level:</dt>
-          <dd>-</dd>
-        </dl>
-        <div className="buttonGroup">
-          <button>♥️ Play</button>
-          <button>🍙 Feed</button>
-          <button>☝🏼 Scold</button>
+      {pets.map(pet => (
+        <div key={pet.id} className="pets">
+          <h2>{pet.name}</h2>
+          <p></p>
+          <dl className="status">
+            <dt className="statusTitle">Birthday:</dt>
+            <dd className="birthday">{pet.birthday}</dd>
+            <dt className="statusTitle">Hunger Level:</dt>
+            <dd>{pet.hungerLevel}</dd>
+            <dt className="statusTitle">Happiness Level:</dt>
+            <dd>{pet.happinessLevel}</dd>
+            <div className="buttonGroup">
+              <button>♥️ Play</button>
+              <button>🍙 Feed</button>
+              <button>☝🏼 Scold</button>
+            </div>
+          </dl>
         </div>
-      </div>
+      ))}
     </>
   )
 }
